6e0ae5c2311d8d764872b8e4569957e35a2132a8
Maria Eduarda Bastos de Mello <92958369+madubmello@users.noreply.github.com>
2024-02-01 17:31:17 -0300

    Update pedra_papel_tesoura.py

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -1,6 +1,6 @@
 import random
 
-print("Seja bem-vinda ao jogo! Essa é uma versão básica de Pedra, Papel e Tesoura de duas rodadas!")
+print("Seja bem-vinda ao jogo! Essa é uma versão básica de Pedra, Papel e Tesoura de três rodadas!")
 rodadas = 3
 pontosPlayer = 0
 pontosComputador = 0
@@ -11,6 +11,7 @@ for rodadas in range(1, rodadas+1,1):
   while jogadaPlayer != "pedra" and jogadaPlayer != "papel" and jogadaPlayer != "tesoura":
     print("Jogada inválida!")
     jogadaPlayer = input("Faça sua jogada (pedra, papel ou tesoura): ").lower() # não esquecer de pedir novamente, se não o loop não funcionará!
+    
   jogadaComputador = random.choice(listaJogadas)
   print(f"A jogada de seu oponente foi {jogadaComputador}.")
 
